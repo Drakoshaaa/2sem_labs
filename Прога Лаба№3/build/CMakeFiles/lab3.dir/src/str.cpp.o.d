9a210ac6d0178c6bde4fee2475c15ca4a1ae5a14
Drakoshaaa <semen.com321@gmail.com>
2026-04-21 17:47:13 +0300

    oops

--- Прога Лаба№3/build/CMakeFiles/lab3.dir/src/str.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/lab3.dir/src/str.cpp.o: \
- /home/drakoshaaa/Coding/C--/Прога\ 2Sem/Прога\ Лаба№3/src/str.cpp \
- /usr/include/stdc-predef.h \
- /home/drakoshaaa/Coding/C--/Прога\ 2Sem/Прога\ Лаба№3/include/defStr.h
